514f93ec5bdc270b7158a2f504870536e8040ace
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2022-07-14 14:08:22 -0400

    2

--- My project (3)/Assets/Script/CalcProgram.cs
+++ My project (3)/Assets/Script/CalcProgram.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class CalcProgram : MonoBehaviour
 {
-    static public float getDist(float x, float y, float z)
+    static public float getDist3D(float x, float y, float z)
     {
-
+        return Mathf.Sqrt(x * x + y * y + z + z);
+    }
+    static public float getAngle2D(float x, float y)
+    {
+        return ;
     }
 }
