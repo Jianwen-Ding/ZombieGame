a6b97fde4f1337efab27fd03a4af0a578b491b54
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2022-07-14 21:29:50 -0400

    gf

--- My project (3)/Assets/Script/CameraFollower.cs
+++ My project (3)/Assets/Script/CameraFollower.cs
@@ -24,6 +24,20 @@ public class CameraFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = CameraRig.centerEyeAnchor.position;
+        gameObject.transform.position = new Vector3(CameraRig.centerEyeAnchor.position.x, CameraRig.centerEyeAnchor.position.y - distanceBelow, CameraRig.centerEyeAnchor.position.z);
+    }
+    protected virtual void UpdateController()
+    {
+
+        var p = CameraRig.transform.localPosition;
+        if (OVRManager.instance.trackingOriginType == OVRManager.TrackingOrigin.EyeLevel)
+        {
+            p.y = OVRManager.profile.eyeHeight - (distanceBelow) + gameObject.transform.position.y;
+        }
+        else if (OVRManager.instance.trackingOriginType == OVRManager.TrackingOrigin.FloorLevel)
+        {
+            p.y = -(distanceBelow) + gameObject.transform.position.y;
+        }
+        CameraRig.transform.localPosition = p;
     }
 }
--- My project (3)/Assets/Script/PlayerMainScript.cs
+++ My project (3)/Assets/Script/PlayerMainScript.cs
@@ -83,8 +83,10 @@ public class PlayerMainScript : MonoBehaviour
         if (OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick) != Vector2.zero)
         {
             float angle = CalcProgram.getAngle2D(OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x, OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).y);
-            rb.velocity = new Vector3(CalcProgram.getVectorFromAngle2D(mainCamera.transform.rotation.x + angle,speed).x, rb.velocity.y, CalcProgram.getVectorFromAngle2D(mainCamera.transform.rotation.x + angle, speed).y);
-            
+            rb.velocity = new Vector3(CalcProgram.getVectorFromAngle2D(mainCamera.transform.rotation.x + angle, speed).x, rb.velocity.y, CalcProgram.getVectorFromAngle2D(mainCamera.transform.rotation.x + angle, speed).y);
+
         }
+        //Establishing controllers
+
     }
 }
