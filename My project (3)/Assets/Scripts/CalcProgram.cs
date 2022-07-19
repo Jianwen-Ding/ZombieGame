4cbadb415c563a9bf9aab1e927e921a803275b7c
jadeandtea <jaden.tian@gmail.com>
2022-07-19 09:42:33 -0400

    Fix Merge Issues

--- My project (3)/Assets/Scripts/CalcProgram.cs
+++ My project (3)/Assets/Scripts/CalcProgram.cs
@@ -9,43 +9,9 @@ public class CalcProgram : MonoBehaviour
     {
         return Mathf.Sqrt(x * x + y * y);
     }
-    static public float getDist2D(Vector2 vector)
-    {
-        return Mathf.Sqrt(vector.x * vector.x + vector.y * vector.y);
-    }
     static public float getDist3D(float x, float y, float z)
     {
         return Mathf.Sqrt(x * x + y * y + z + z);
-    }
-    static public float getDist3D(Vector3 point)
-    {
-        return Mathf.Sqrt(point.x * point.x + point.y * point.y + point.z * point.z);
-    }
-    static public float getDistBetweenPoints2D(float x, float y, float x1, float y1)
-    {
-        float xDif = x - x1;
-        float yDif = y - y1;
-        return Mathf.Sqrt(xDif * xDif + yDif * yDif);
-    }
-    static public float getDistBetweenPoints2D(Vector2 vector, Vector2 origin)
-    {
-        float xDif = vector.x - origin.x;
-        float yDif = vector.y - origin.y;
-        return Mathf.Sqrt(xDif * xDif + yDif * yDif);
-    }
-    static public float getDistBetweenPoints3D(float x, float y, float z, float x1, float y1, float z1)
-    {
-        float xDif = x - x1 ;
-        float yDif = y - y1;
-        float zDif = z - z1;
-        return Mathf.Sqrt(xDif * xDif + yDif * yDif + zDif + zDif);
-    }
-    static public float getDistBetweenPoints3D(Vector3 vector, Vector3 origin)
-    {
-        float xDif = vector.x - origin.x;
-        float yDif = vector.y - origin.y;
-        float zDif = vector.z - origin.z;
-        return Mathf.Sqrt(xDif * xDif + yDif * yDif + zDif + zDif);
     }
     static public Vector2 getVectorFromAngle2D(float angle, float distance)
     {
@@ -58,25 +24,14 @@ public class CalcProgram : MonoBehaviour
     {
         Vector3 ret;
         ret.x = getVectorFromAngle2D(angleX, distance).x;
-        ret.y = Mathf.Sin(angleY * Mathf.Deg2Rad) * distance; 
-        ret.z = getVectorFromAngle2D(angleX, distance).y;
-        return ret;
-    }
-    static public Vector2 DegreesFromVector(Vector3 angle)
-    {
-        Vector2 ret;
-        ret.x = getAngle2D(angle.x, angle.z);
-        ret.y = getAngle2D(angle.z, angle.y);
+        ret.y = getVectorFromAngle2D(angleX, distance).y;
+        ret.z = Mathf.Sin(angleY * Mathf.Deg2Rad) * distance;
         return ret;
     }
     //Output is in degree
     static public float getAngle2D(float x, float y)
     {
         return Mathf.Rad2Deg * Mathf.Atan2(y, x);
-    }
-    static public float getAngle2D(Vector2 vector)
-    {
-        return Mathf.Rad2Deg * Mathf.Atan2(vector.y, vector.x);
     }
     static public float getAngleBetweenPoints2D(float x, float y, float x2, float y2)
     {
@@ -85,41 +40,13 @@ public class CalcProgram : MonoBehaviour
         return getAngle2D(xDif, yDif);
         
     }
-    static public float getAngleBetweenPoints2D(Vector2 vector, Vector2 origin)
-    {
-        float xDif = vector.x - origin.x;
-        float yDif = vector.y - origin.y;
-        return getAngle2D(xDif, yDif);
-
-    }
     //angleX
     //angleY
-    static public Vector2 getAngle3D(float x, float y, float z)
+    static public Vector2 getAngleDiffrence3D(float x, float y, float z, float x2, float y2, float z2)
     {
         Vector2 ret;
         ret.x = Mathf.Rad2Deg * Mathf.Atan2(z, x);
         ret.y = Mathf.Rad2Deg * Mathf.Atan2(y, z);
-        return ret;
-    }
-    static public Vector2 getAngle3D(Vector3 vector)
-    {
-        Vector2 ret;
-        ret.x = Mathf.Rad2Deg * Mathf.Atan2(vector.z, vector.x);
-        ret.y = Mathf.Rad2Deg * Mathf.Atan2(vector.y, vector.z);
-        return ret;
-    }
-    static public Vector2 getAngleBetweenPoints3D(float x, float y, float z, float x1, float y1, float z1)
-    {
-        Vector2 ret;
-        ret.x = Mathf.Rad2Deg * Mathf.Atan2(z - z1, x - x1);
-        ret.y = Mathf.Rad2Deg * Mathf.Atan2(y - y1, z - z1);
-        return ret;
-    }
-    static public Vector2 getAngleBetweenPoints3D(Vector3 vector, Vector3 origin)
-    {
-        Vector2 ret;
-        ret.x = Mathf.Rad2Deg * Mathf.Atan2(vector.z - origin.z, vector.x - origin.x);
-        ret.y = Mathf.Rad2Deg * Mathf.Atan2(vector.y - origin.y, vector.z - origin.z);
         return ret;
     }
 }
--- My project (3)/Assets/Scripts/ScytheState.cs
+++ My project (3)/Assets/Scripts/ScytheState.cs
@@ -5,15 +5,11 @@ using UnityEngine;
 public class ScytheState : MonoBehaviour
 {
     //establish
-    [SerializeField]
     BoxCollider collide;
-    [SerializeField]
-    GameObject grabbedBy;
+    OVRGrabbable grab;
     Rigidbody rb;
-    [SerializeField]
-    OVRCameraRig camRig;
-    [SerializeField]
-    Vector3 setSize;
+    bool isRight;
+    
     //States
     //"grabbed"
     //"flying"
@@ -26,172 +22,63 @@ public class ScytheState : MonoBehaviour
     //flying scytheState
     //
     [SerializeField]
-    bool xChangePositive;
-    [SerializeField]
-    Vector3 lockedVelocity;
-    [SerializeField]
     float zAxisRotateOnRelease;
-    [SerializeField]
-    float yAxisRotateOnRelease;
     [SerializeField]
     float xAxisChange;
     [SerializeField]
     float speedToSpinRatio;
     [SerializeField]
     float rotationDebug;
-    [SerializeField]
-    float damageThrowDeal;
-    [SerializeField]
-    float minimumThrowSpeed;
-    [SerializeField]
-    float throwSpeedMultiplier;
     //stuck scytheState
     GameObject stabbedObject;
-    [SerializeField]
-    float hitImpulse;
-    //slash
-    [SerializeField]
-    bool isSlashing;
-    [SerializeField]
-    float knockBackSlash;
-    [SerializeField]
-    int damageOnSlash;
-    //Size of hitbox
-    [SerializeField]
-    Vector3 slashSizeIncrease;
-    [SerializeField]
-    Vector3 slashOriginalSize;
-    public void setLockedVel(Vector3 lockSet)
-    {
-        lockedVelocity = lockSet;
-    }
-    public Vector3 getLockedVel()
-    {
-        return lockedVelocity;
-    } 
-    public void setSlash(bool set)
-    {
-        isSlashing = set;
-    }
-    public bool getSlash()
-    {
-        return isSlashing;
-    }
-    public void establishGrabbedBy(GameObject establish)
-    {
-        grabbedBy = establish;
-    }
-    public void establishGrab()
-    {
-        rb.isKinematic = true;
-        scytheState = "grabbed";
-        gameObject.transform.localScale = setSize;
-    }
-    public void endGrab(Vector3 linearVelocity, Vector3 angularVelocity)
-    {
-        scytheState = "flying";
-        float speed = CalcProgram.getDist3D(linearVelocity);
-        Vector2 angles = CalcProgram.getAngle3D(linearVelocity);
-        speed *= throwSpeedMultiplier;
-        if(speed < minimumThrowSpeed)
-        {
-            lockedVelocity = CalcProgram.getVectorFromAngle3D(angles.x , angles.y, minimumThrowSpeed);
-        }
-        else
-        {
-            lockedVelocity = CalcProgram.getVectorFromAngle3D(angles.x, angles.y, speed);
-        }
-        rb.velocity = lockedVelocity;
-        if(angularVelocity.x > 0)
-        {
-            xChangePositive = true;
-        }
-        else
-        {
-            xChangePositive = false;
-        }
-        rb.angularVelocity = Vector3.zero;
-        zAxisRotateOnRelease = gameObject.transform.rotation.z;
-        xAxisChange = gameObject.transform.rotation.x;
-        yAxisRotateOnRelease = gameObject.transform.rotation.y;
-    }
     // Start is called before the first frame update
     void Start()
     {
+        //establish
         collide = gameObject.GetComponent<BoxCollider>();
-        slashOriginalSize = collide.size;
-        setSize = gameObject.transform.localScale;
-        OVRCameraRig[] CameraRigs = GameObject.FindGameObjectWithTag("Player").GetComponentsInChildren<OVRCameraRig>();
-
-        if (CameraRigs.Length == 0)
-            Debug.LogWarning("OVRPlayerController: No OVRCameraRig attached.");
-        else if (CameraRigs.Length > 1)
-            Debug.LogWarning("OVRPlayerController: More then 1 OVRCameraRig attached.");
-        else
-            camRig = CameraRigs[0];
         rb = gameObject.GetComponent<Rigidbody>();
+        grab = gameObject.GetComponent<OVRGrabbable>();
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag != "Hands" && scytheState == "flying" &&  other.gameObject.layer != 6)
+        if(scytheState == "flying" && other.gameObject.tag != "Hands")
         {
-            stabbedObject = other.gameObject;
+            gameObject.transform.parent = other.gameObject.transform;
             scytheState = "stuck";
             rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-            rb.isKinematic = false;
-            if (other.gameObject.GetComponent<Rigidbody>() != null)
-            {
-                other.gameObject.GetComponent<Rigidbody>().AddForce(lockedVelocity * hitImpulse, ForceMode.Impulse);
-            }
         }
-        if(other.gameObject.tag != "Hands" && isSlashing && other.gameObject.layer != 6)
-        {
-            if(other.gameObject.GetComponent<Rigidbody>() != null)
-            {
-                Vector2 angles = CalcProgram.getAngleBetweenPoints3D(transform.position, camRig.centerEyeAnchor.position);
-                other.gameObject.GetComponent<Rigidbody>().AddForce(CalcProgram.getVectorFromAngle3D(angles.x, angles.y, knockBackSlash), ForceMode.Impulse);
-            }
-           
-        }
+        
     }
     // Update is called once per frame
     void Update()
     {
-        if (scytheState == "grabbed")
+       if(grab.grabbedBy != null)
         {
+            scytheState = "grabbed";
+            gameObject.transform.parent = null;
+            collide.isTrigger = false;
+        }
+       if(scytheState == "grabbed")
+        {
+            rb.useGravity = true;
+            if(grab.grabbedBy == null)
+            {
+                scytheState = "flying";
+            }
+        }
+       if(scytheState == "flying")
+        {
+            collide.isTrigger = true;
+            rb.useGravity = false;
+            float speed = Mathf.Abs(CalcProgram.getDist3D(rb.velocity.x , rb.velocity.y, rb.velocity.z));
+            xAxisChange += speed * speedToSpinRatio * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(xAxisChange, transform.rotation.y, zAxisRotateOnRelease);
+            
             
         }
-        if(scytheState == "flying")
+        if (scytheState == "stuck")
         {
-            rb.isKinematic = false;
-            rb.velocity = lockedVelocity;
-            rb.angularVelocity = Vector3.zero;
-            gameObject.transform.rotation = Quaternion.Euler(new Vector3(xAxisChange, yAxisRotateOnRelease, zAxisRotateOnRelease));
-            if (xChangePositive)
-            {
-                xAxisChange += Time.deltaTime * CalcProgram.getDist3D(lockedVelocity) * speedToSpinRatio;
-            }
-            else
-            {
-                xAxisChange -= Time.deltaTime * CalcProgram.getDist3D(lockedVelocity) * speedToSpinRatio;
-            }
-            
-        }
-        if(scytheState == "stuck")
-        {
-        }
-        else
-        {
-            gameObject.transform.localScale = setSize;
-        }
-        if (isSlashing)
-        {
-            collide.size = slashSizeIncrease;
-        }
-        else
-        {
-            collide.size = slashOriginalSize;
+            rb.velocity = Vector3.zero;
         }
     }
 }
