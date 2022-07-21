a7934505baedc3ed178d9e959dbff0c9158a8685
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2022-07-21 10:50:07 -0400

    Music script start

---
+++ My project (3)/Assets/Script/MusicScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+    public static MusicScript soleMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicScript[] allMusic = 
+        if()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
